3a2315aae2cc90385e055562c660dcc5f0cbe296
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-07-20 14:09:04 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import random
 first_names=('Shani','Feni','Shania')
 last_names=('Indira','Fitriyanti','Gracia')
-user=random.choice(first_names)+random.choice(last_names)
+member=random.choice(first_names)+random.choice(last_names)
+user=member
