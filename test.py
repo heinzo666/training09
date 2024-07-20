876b6fb3d7be8dcb51fd72f62e9a5d925ec95c97
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-07-20 14:30:20 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,4 @@ import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
 os.chdir('/content/training')
 
-full_local_path = "/content/training"
-
-repo = Repo(full_local_path)
-repo.git.add("-A")
 
